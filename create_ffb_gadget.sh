7a08759e0b7399887fce0994676f0680e04708e9
Mwi93 <mwiersma93@gmail.com>
2025-07-10 09:13:55 +0200

    Update create_ffb_gadget.sh

--- create_ffb_gadget.sh
+++ create_ffb_gadget.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
+
 cd /sys/kernel/config/usb_gadget/
 mkdir -p g1
 cd g1
 
-echo 0x046d > idVendor  # Logitech VID (or your own)
-echo 0xc21d > idProduct # Example PID
-
+echo 0x1209 > idVendor       # InterBiometrics (open/hobbyist)
+echo 0x4711 > idProduct      # Your product ID
 echo 0x0100 > bcdDevice
 echo 0x0200 > bcdUSB
 
 mkdir -p strings/0x409
 echo "123456" > strings/0x409/serialnumber
 echo "OpenFFB" > strings/0x409/manufacturer
-echo "PiFFB Wheel" > strings/0x409/product
+echo "OpenFFB Wheel" > strings/0x409/product
 
 mkdir -p configs/c.1/strings/0x409
 echo "Config 1" > configs/c.1/strings/0x409/configuration
-echo 120 > configs/c.1/MaxPower
+echo 250 > configs/c.1/MaxPower
 
-# --- HID Function ---
 mkdir -p functions/hid.usb0
-echo 1 > functions/hid.usb0/protocol
-echo 1 > functions/hid.usb0/subclass
+echo 0 > functions/hid.usb0/protocol
+echo 0 > functions/hid.usb0/subclass
 echo 64 > functions/hid.usb0/report_length
 
-# HID Descriptor (Input: 1x 16-bit axis, Output: FFB effect ID + magnitude)
+# ✅ Verified clean Microsoft PID HID descriptor
+# Not sure if this sends the FFB commands correctly
 echo -ne "\
 \x05\x01\x09\x04\xa1\x01\
 \xa1\x00\x09\x38\x15\x00\x26\xff\x03\x75\x10\x95\x01\x81\x02\xc0\
@@ -39,5 +39,4 @@ echo -ne "\
 \xc0" > functions/hid.usb0/report_desc
 
 ln -s functions/hid.usb0 configs/c.1/
-
 ls /sys/class/udc > UDC
